58f2788a05f14e3972d15fe2aea9048e351c00ce
soe-sandar-kyi <soesandarkyi22@gmail.com>
2025-01-19 15:36:55 +0630

    feat: ado.net create, edit, update, delete

---
+++ DotNetCoreTraining.ConsoleApp/AdoDotNetCRUD.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTraining.ConsoleApp
+{
+    public class AdoDotNetCRUD
+    {
+        private readonly string _connectionString = "Data Source=DESKTOP-M44SRQR;Initial Catalog=DotNetTrainingBatch5;Integrated Security = True;TrustServerCertificate=True";
+        public void Read()
+        {
+            Console.WriteLine("Connection string" + _connectionString);
+            SqlConnection connection = new SqlConnection(_connectionString);
+
+            Console.WriteLine("Connection openning.....");
+            connection.Open();
+            Console.WriteLine("Connection opened successfully");
+
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Blog] where DeleteFlag=0";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+                Console.WriteLine(reader["DeleteFlag"]);
+            }
+
+            Console.WriteLine("Connection closing.....");
+            connection.Close();
+            Console.WriteLine("Connection closed successfully");
+
+            //DataSet
+            //DataTable
+            //DataRow
+            //DataColumn
+            //foreach(DataRow dr in dataTable.Rows)
+            //{
+            // Console.WriteLine(dr["BlogTitle"]);
+            // Console.WriteLine(dr["BlogAuthor"]);
+            //Console.WriteLine(dr["BlogContent"]);
+            //Console.WriteLine(dr["DeleteFlag"]);
+            //}
+        }
+        public void Create()
+        {
+            Console.Write("Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Blog Content: ");
+            string content = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = $@"INSERT INTO [dbo].[Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+            SqlCommand cmd2 = new SqlCommand(query, connection);
+            cmd2.Parameters.AddWithValue("@BlogTitle", title);
+            cmd2.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd2.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd2.ExecuteNonQuery();
+
+
+            Console.WriteLine(result == 1 ? "Data inserted successfully" : "Data not inserted");
+            connection.Close();
+        }
+
+        public void Edit()
+        {
+            Console.Write("Enter Blog Id: ");
+            string blogId = Console.ReadLine();
+
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Blog] where BlogId=@BlogId";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@BlogId", blogId);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            if (dataTable.Rows.Count == 0)
+            {
+                Console.WriteLine("No data found");
+                return;
+            }
+
+            DataRow dr = dataTable.Rows[0];
+            Console.WriteLine("Blog Id: " + dr["BlogId"]);
+            Console.WriteLine("Blog Title: " + dr["BlogTitle"]);
+            Console.WriteLine("Blog Author: " + dr["BlogAuthor"]);
+            Console.WriteLine("Blog Content: " + dr["BlogContent"]);
+        }
+        public void Update()
+        {
+            Console.Write("Enter Blog Id: ");
+            int blogId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Blog Content: ");
+            string content = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"UPDATE [dbo].[Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+      ,[DeleteFlag] = 0
+ WHERE BlogId=@BlogId";
+
+            SqlCommand cmd2 = new SqlCommand(query, connection);
+            cmd2.Parameters.AddWithValue("@BlogId", blogId);
+            cmd2.Parameters.AddWithValue("@BlogTitle", title);
+            cmd2.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd2.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd2.ExecuteNonQuery();
+            Console.WriteLine(result == 1 ? "Data updated successfully" : "Data not updateds");
+            connection.Close();
+        }
+        public void Delete()
+        {
+            Console.Write("Enter Blog Id: ");
+            int blogId = Convert.ToInt32(Console.ReadLine());
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"UPDATE [dbo].[Blog]
+   SET [DeleteFlag] = 1
+ WHERE BlogId=@BlogId";
+
+            SqlCommand cmd2 = new SqlCommand(query, connection);
+            cmd2.Parameters.AddWithValue("@BlogId", blogId);
+            int result = cmd2.ExecuteNonQuery();
+            Console.WriteLine(result == 1 ? "Data Deleted successfully" : "Data not deleted");
+            connection.Close();
+        }
+
+        public void DeletePermanently()
+        {
+            Console.Write("Enter Blog Id: ");
+            int blogId = Convert.ToInt32(Console.ReadLine());
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"DELETE FROM [dbo].[Blog]
+      WHERE BlogId=@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", blogId);
+            int result = cmd.ExecuteNonQuery();
+            Console.WriteLine(result == 1 ? "Data Deleted successfully" : "Data not deleted");
+            connection.Close();
+        }
+    }
+}
--- DotNetCoreTraining.ConsoleApp/Program.cs
+++ DotNetCoreTraining.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DotNetCoreTraining.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -8,47 +9,22 @@ Console.WriteLine("Hello, World!");
 //Console.ReadKey();
 //md => markdown
 
-string connectionString = "Data Source=DESKTOP-M44SRQR;Initial Catalog=DotNetTrainingBatch5;Integrated Security = True;TrustServerCertificate=True";
-Console.WriteLine("Connection string"+connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
+AdoDotNetCRUD adoDotNetCRUD = new AdoDotNetCRUD();
 
-Console.WriteLine("Connection openning.....");
-connection.Open();
-Console.WriteLine("Connection opened successfully");
+//Read data
+//adoDotNetCRUD.Read();
 
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Blog] where DeleteFlag=0";
+//Insert data
+//adoDotNetCRUD.Create();
 
-SqlCommand command = new SqlCommand(query, connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(command);
-//DataTable dataTable = new DataTable();
-//adapter.Fill(dataTable);
+//Read data by id
+//adoDotNetCRUD.Edit();
 
-SqlDataReader reader= command.ExecuteReader();
-while(reader.Read())
-{
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-    Console.WriteLine(reader["DeleteFlag"]);
-}
+//Uodate data
+//adoDotNetCRUD.Update();
 
-Console.WriteLine("Connection closing.....");
-connection.Close();
-Console.WriteLine("Connection closed successfully");
+//Set DeleteFlag=1
+//adoDotNetCRUD.Delete();
 
-//DataSet
-//DataTable
-//DataRow
-//DataColumn
-//foreach(DataRow dr in dataTable.Rows)
-//{
-   // Console.WriteLine(dr["BlogTitle"]);
-   // Console.WriteLine(dr["BlogAuthor"]);
-    //Console.WriteLine(dr["BlogContent"]);
-    //Console.WriteLine(dr["DeleteFlag"]);
-//}
+//Delete data
+adoDotNetCRUD.DeletePermanently();
